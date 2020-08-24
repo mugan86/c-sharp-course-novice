1c5d09c6d51c62e0e31af8d49329698085367da1
Anartz Mugika Ledo <mugan86@gmail.com>
2020-08-24 20:34:33 +0200

    Sección 1 / 2 Completados

---
+++ Practica/2-manejo-mensajes-pantalla/EjercicioFinal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EjercicioFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Introducir la información
+            Console.WriteLine("Nombre: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Apellidos: ");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Edad: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hobby: ");
+            string hobby = Console.ReadLine();
+            // Mostrar la información 
+            Console.WriteLine("=========INFORMACIÓN BÁSICA=========");
+            Console.WriteLine($"Eres {name} {lastname}.");
+            Console.WriteLine($"Tienes {age} años.");
+            Console.WriteLine($"Hobby: { hobby }");
+            Console.WriteLine("====================================");
+        }
+    }
+}
---
+++ Teoria/2-manejo-mensajes-pantalla/MensajePant/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MensajePant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Trabajando con la función Write
+            Console.Write("Hello World!  ");
+            Console.Write("Hello World! ");
+            Console.Write("   Hello World!");
+            Console.Write("Hello World!");
+            Console.Write("Hello World!");
+            // Trabajando con la función WriteLine
+            Console.WriteLine("Comenzamos con el apartado de WriteLine");
+            Console.WriteLine("--e--e-e-e");
+            Console.WriteLine("ULTIMO");
+            // Trabajando con la función ReadLine
+            Console.WriteLine("Introduzca tu edad: ");
+            Console.WriteLine($"Mi edad es {Console.ReadLine()}");
+        }
+    }
+}
