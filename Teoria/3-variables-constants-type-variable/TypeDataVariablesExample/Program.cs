eaad73f1bffd52ad0e9bc1d5cd0042f8169872d0
Anartz Mugika Ledo <mugan86@gmail.com>
2020-09-06 09:27:25 +0200

    Tipos de datos numéricos - enteros - Con Signo

---
+++ Teoria/3-variables-constants-type-variable/TypeDataVariablesExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TypeDataVariablesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tipos de datos numéricos - Enteros - Con signo");
+            // sbyte
+            // sbyte sbyteInvalid = 130; <--- ERROR sería un short
+            sbyte sbyteValid = 120;
+            Console.WriteLine(sbyteValid);
+            // short 
+            // short shortValue = 3323939; <-- ERROR sería un int
+            short shortValue = 23939;
+            Console.WriteLine(shortValue);
+
+            // int 
+            // int intValue = 332393944444444; // <-- ERROR sería un int
+            shortValue = 3333;
+            Console.WriteLine(shortValue);
+            int intValue = 333344444444444;
+            Console.WriteLine(intValue);
+
+            
+            
+        }
+    }
+}
