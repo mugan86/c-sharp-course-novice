445bae4d2ca0fa1c27a97c44dc81c7046f161671
Anartz Mugika Ledo <mugan86@gmail.com>
2020-09-06 09:28:38 +0200

    Tipos de datos numéricos Enteros SIN SIGNO

--- Teoria/3-variables-constants-type-variable/TypeDataVariablesExample/Program.cs
+++ Teoria/3-variables-constants-type-variable/TypeDataVariablesExample/Program.cs
@@ -6,7 +6,7 @@ namespace TypeDataVariablesExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tipos de datos numéricos - Enteros - Con signo");
+            /*Console.WriteLine("Tipos de datos numéricos - Enteros - Con signo");
             // sbyte
             // sbyte sbyteInvalid = 130; <--- ERROR sería un short
             sbyte sbyteValid = 120;
@@ -21,10 +21,24 @@ namespace TypeDataVariablesExample
             shortValue = 3333;
             Console.WriteLine(shortValue);
             int intValue = 333344444444444;
-            Console.WriteLine(intValue);
+            Console.WriteLine(intValue);*/
 
-            
-            
+            Console.WriteLine("Tipos de datos numéricos - Enteros - SIN signo");
+            // byte
+            // byte byteInvalid = 256; // <--- ERROR sería un ushort (POR fuera de rango)
+            byte byteValid = 120;
+            Console.WriteLine(byteValid);
+            // ushort 
+            // ushort shortValue = 65536; // <-- ERROR sería un uint (Fuera de rango)
+            ushort ushortValue = 1234;
+            Console.WriteLine(ushortValue);
+
+            // uint 
+            uint intValue = 4294967295; // <-- ERROR sería un int
+            // int intInvalidValue = -23; // <-- ERROR sería un int
+            Console.WriteLine(intValue);
+            // int intValue = 333344444444444;
+            // Console.WriteLine(intValue);
         }
     }
 }
