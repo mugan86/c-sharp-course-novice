f61151e0d998f0cdfdf973cee5db8bd564b953e3
Anartz Mugika Ledo <mugan86@gmail.com>
2020-09-06 09:29:56 +0200

    Tipos de datos Numericos NO ENTEROS

--- Teoria/3-variables-constants-type-variable/TypeDataVariablesExample/Program.cs
+++ Teoria/3-variables-constants-type-variable/TypeDataVariablesExample/Program.cs
@@ -23,7 +23,7 @@ namespace TypeDataVariablesExample
             int intValue = 333344444444444;
             Console.WriteLine(intValue);*/
 
-            Console.WriteLine("Tipos de datos numéricos - Enteros - SIN signo");
+            /*Console.WriteLine("Tipos de datos numéricos - Enteros - SIN signo");
             // byte
             // byte byteInvalid = 256; // <--- ERROR sería un ushort (POR fuera de rango)
             byte byteValid = 120;
@@ -38,7 +38,14 @@ namespace TypeDataVariablesExample
             // int intInvalidValue = -23; // <-- ERROR sería un int
             Console.WriteLine(intValue);
             // int intValue = 333344444444444;
-            // Console.WriteLine(intValue);
+            // Console.WriteLine(intValue);*/
+
+            Console.WriteLine("Tipos de datos numéricos - NO Enteros");
+            // double floatValue = 120202020239393939312020202023939391202020202393939393933333333333333333332120202020239393939393333333333333333333239393333333333333333333293333333333333333333212020202023939393939333333333333333333321202020202393939393933333333333333333332120202020239393939393333333333333333333212020202023939393939333333333333333333321202020202393939393933333333333333333332120202.03d;
+            // SIN ERROR
+            double floatValue = 1202020202393939393120202020239393912020202023939393939333333333333333333321202020202393939393933333333333333333332393933333333333333333332933333333333333333332120202020239393939393333333333333333333212020202023939393939333333333333333333321202020202393939393933333333332.03d;
+            Console.WriteLine(floatValue);
+
         }
     }
 }
