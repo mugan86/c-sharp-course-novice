c27f39296968cceed1ed81a2904e605661ef7cd0
Anartz Mugika Ledo <mugan86@gmail.com>
2020-09-10 23:42:50 +0200

    Operadores - Type Casting

---
+++ Teoria/3-variables-constants-type-variable/OperatorsAritmetic/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace OperatorsAritmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Sumas (+)");
+            Console.WriteLine( $"5 + 4 + 56 + 34 = {5 + 4 + 56 + 34}"); // 99
+            Console.WriteLine( $"5.34d + 4.34d + 56 + 34 = {5.34d + 4.34d + 56 + 34}"); // 99.68
+            Console.WriteLine( $"(-25) + 4 + (-56) + 34 = {(-25) + 4 + (-56) + 34}"); // -43
+            Console.WriteLine("Restas (-)");
+            Console.WriteLine( $"5 - 4 - 6 - 4 = {5 - 4 - 6 -4}"); // -9
+            Console.WriteLine( $"5.34d - 4.34d -1.5d = {5.34d - 4.34d -1.5d}"); // -0.5
+            Console.WriteLine( $"(-25.5f) + 4.5f + (-56) + 34 = {(-25.5f) + 4.5f + (-56) + 34}"); // -43
+            
+            
+            
+            
+            
+            
+            Console.WriteLine("Multiplicación (-)");
+            Console.WriteLine( $"5 * 4 * 6 = {5 * 4 * 6}"); // 120
+            Console.WriteLine( $"5 * 4 * -6 = {5 * 4 * (-6)}"); // -120
+            Console.WriteLine( $"5 * (-4) * (-6) = {5 * (-4) * (-6)}"); // 120
+            Console.WriteLine( $"2.5f * 5 = {2.5f * 5}"); // 12.5
+            Console.WriteLine( $"2.5f * -5 = {2.5f * -5}"); // -12.5
+
+
+
+
+            Console.WriteLine("Combinar operadores (+, -, *)");
+            Console.WriteLine( $"((5 * 4) + 65 -45) * 5 = {((5 * 4) + 65 -45) * 5}"); // 200
+            Console.WriteLine( $"5 * 4 + 65 -45 * 5 = {5 * 4 + 65 -45 * 5}"); // -140
+
+
+            Console.WriteLine("Divisiones (/)");
+            Console.WriteLine( $"20 / 5 = {20 / 5}"); // 4
+            Console.WriteLine( $"20 / 5 / 5d = {20 / 5 / 5d}"); // 0.8
+            Console.WriteLine( $"(-20) / (-5) / 5d = {(-20) / (-5) / 5d}"); // 0.8
+            Console.WriteLine( $"(-20) / (5) / 5d = {(-20) / (5) / 5d}"); // -0.8
+            Console.WriteLine( $"(-20) / (-5) / (-5d) = {(-20) / (-5) / (-5d)}"); // -0.8
+            Console.WriteLine( $"(-20) / (-5) / (-5d) / (-4) = {(-20) / (-5) / (-5d) / (-4)}"); // 0.2
+        
+
+
+            Console.WriteLine("Módulo (resto) (%)");
+            Console.WriteLine( $"20 % 5 = {20 % 5}"); // 0
+            Console.WriteLine( $"20 % 6 = {20 % 6}"); // 2*/
+
+            Console.WriteLine("Combinar operadores (+, -, *, / , %)");
+            Console.WriteLine( $"(((5 * 4) + 65 -45) * 5) % 40 = {(((5 * 4) + 65 -45) * 5) % 40 }"); // 0
+        }
+    }
+}
---
+++ Teoria/3-variables-constants-type-variable/TypeCasting/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TypeCasting
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tipo de conversión implícito");
+            // char -> int -> long -> float -> double
+            char character = 'a';
+            int characterInt = character;
+            Console.WriteLine($"Estamos convirtiendo el carácter '{character}' a un valor entero: {characterInt}");
+            character = 'm';
+            characterInt = character;
+            Console.WriteLine($"Estamos convirtiendo el carácter '{character}' a un valor entero: {characterInt}");
+            // int => double / int => float
+            int numberInt = 99;
+            double numberDouble = numberInt;
+            float numberFloat = numberInt;
+            Console.WriteLine($"Número entero {numberInt} => float {numberFloat} => double {numberDouble}");
+            // Vamos a intentar convertir int en char
+            int aCharacterInt = 97;
+            char a = (char) aCharacterInt; // Solucionado con la conversión explícito
+
+            Console.WriteLine("Tipo de conversión explícita");
+            // double -> float -> long -> int -> char
+            double valorInicial = 97.8d;
+            Console.WriteLine($"Estamos convirtiendo el valor (double) '{valorInicial}' a un valor float: {(float)valorInicial}");
+            Console.WriteLine($"Estamos convirtiendo el valor (double) '{valorInicial}' a un valor long: {(long)valorInicial}");
+            Console.WriteLine($"Estamos convirtiendo el valor (double) '{valorInicial}' a un valor char: {(char)valorInicial}");
+            Console.WriteLine("Tipo de conversión con clase Convert");
+            // Aquí añadimos la referencia de la documentación: URL
+            // https://docs.microsoft.com/es-es/dotnet/api/system.convert?view=netcore-3.1
+            int myInt = 10;
+            double myDouble = 56.234d;
+            bool myBool = true;
+            char myChar = 'a';
+            Console.WriteLine(Convert.ToString(myInt)); // "10"
+            Console.WriteLine(Convert.ToDouble(myInt)); // 10
+            Console.WriteLine(Convert.ToInt32(myChar)); // 97
+            Console.WriteLine(Convert.ToString(myBool)); // "True"
+            Console.WriteLine(Convert.ToInt64(myDouble)); // 56
+            Console.WriteLine(Convert.ToBoolean(0)); // False
+            Console.WriteLine(Convert.ToBoolean(myDouble)); // True
+        }
+    }
+}
