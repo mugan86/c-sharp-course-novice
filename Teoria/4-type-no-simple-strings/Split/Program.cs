3098ebec4db365a95420b31bf106e4c39a46e2ce
Anartz Mugika Ledo <mugan86@gmail.com>
2020-09-17 07:22:07 +0200

    Terminado el apartado sección 4 y añadido ejercicio básico de Arrays en dos partes

---
+++ Teoria/4-type-no-simple-strings/Split/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterVideoGames = "Mario/Luigi/Sack Boy/Lara Corft/Solid Snake/Sonic";
+            string [] charactersList = characterVideoGames.Split('/');
+            // Coger string con varios caracteres separando
+            string message = "¡Hola Anartz! ¿Qué tal estás? (Espero que todo bien.)";
+            char [] separatorList = {'¡', '¿', '('};
+            string [] messageList = message.Split(separatorList);
+        }
+    }
+}
---
+++ Teoria/5-collections/Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Primera parte de los arrays.
+            // Obtener array a partir de un string separando por espacios
+            string message = "Estamos trabajando en el curso de C#";
+            string[] messages = message.Split(' ');
+            // Acceder a la info del primero, último y segundo elemento
+            string first = messages[0];
+            string last = messages[messages.Length - 1];
+            string second = messages[1];
+            // Segunda parte modificación de los elementos 0, 1, 2, la última
+            messages[0] = "Estoy";
+            messages[messages.Length - 1] = "Anartz";
+            messages[1] = "aprendiendo";
+            messages[2] = "con";
+            string messageFinal = $"{messages[0]} {messages[1]} {messages[2]} {messages[3]} {messages[4]} {messages[5]} {messages[6]}";
+            Console.WriteLine(messageFinal); // Estoy aprendiendo con el curso de Anartz
+        }
+    }
+}
