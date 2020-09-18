08c52779cf388bd76f534881bfab6c90e0f254e3
Anartz Mugika Ledo <mugan86@gmail.com>
2020-09-18 21:19:10 +0200

    Actualizar con nuevos ejercicios

---
+++ Teoria/5-collections/ArrayReverse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ArrayReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] numbersInt = {1,2,3,4,5};
+
+            Array.Reverse(numbersInt);
+
+            // Modificar algunos valores
+            numbersInt[2] = 456;
+            numbersInt[3] = 35280;
+
+            Array.Reverse(numbersInt, 2, numbersInt.Length - 2);
+
+        }
+    }
+}
---
+++ Teoria/5-collections/ArraySort/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArraySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string shoppingCartList = "Cola,Platano,Naranja,Filete,Legumbres";
+            // Arrays
+            string [] shoppingCartArray = shoppingCartList.Split(',');
+            int [] coinsList = {100, 1, 5, 2, 1000, 750};
+
+            Array.Sort(shoppingCartArray);
+            shoppingCartArray[2] = "Sopa";
+            Array.Sort(shoppingCartArray);
+
+            Array.Sort(coinsList);
+            coinsList[3] = 0;
+            Array.Sort(coinsList);
+            coinsList[0] = 4503;
+            coinsList[3] = 7399;
+            Array.Sort(coinsList, 3, coinsList.Length - 3);
+        }
+    }
+}
