cdedb500d98867f0fe4e9a3c45a96c478def5dab
Anartz Mugika Ledo <mugan86@gmail.com>
2020-09-21 21:10:33 +0200

    Finish if structure

--- Teoria/5-control-structures/IfStructure/Program.cs
+++ Teoria/5-control-structures/IfStructure/Program.cs
@@ -7,26 +7,32 @@ namespace IfStructure
         static void Main(string[] args)
         {
             // if simple
-            /*Console.WriteLine("Introduce un número del 1 al 10");
+            Console.WriteLine("Introduce un número del 1 al 10");
             int numberInput = Convert.ToInt32(Console.ReadLine());
-            if (numberInput > 10) {
+            if (numberInput > 10)
+            {
                 Console.WriteLine("Has introducido el siguiente número {0} y es mayor que 10", numberInput);
             }
-            if (numberInput > 10 || numberInput < 0) {
+            if (numberInput > 10 || numberInput < 0)
+            {
                 Console.WriteLine("No has escrito un valor válido");
             }
 
-            if (numberInput <= 10 && numberInput > 0) { // 1-10
+            if (numberInput <= 10 && numberInput > 0)
+            { // 1-10
                 Console.WriteLine("Estás en el rango");
-            }*/
+            }
 
             // If else
-            /*Console.WriteLine("Escribe tu nombre: ");
+            Console.WriteLine("Escribe tu nombre: ");
             string name = Console.ReadLine();
 
-            if (name == "Anartz") {
+            if (name == "Anartz")
+            {
                 Console.WriteLine("Te llamas Anartz");
-            } else {
+            }
+            else
+            {
                 Console.WriteLine("Oye, tu no eres Anartz. Eres {0}", name);
             }
 
@@ -37,17 +43,25 @@ namespace IfStructure
             int numberTwo = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Número 3: ");
             int numberThree = Convert.ToInt32(Console.ReadLine());
-            if (name == "Anartz" && (numberOne < numberTwo || numberTwo > numberThree)) {
+            if (name == "Anartz" && (numberOne < numberTwo || numberTwo > numberThree))
+            {
                 Console.WriteLine("Cumple las condiciones númericas y del nombre");
-            } else {
+            }
+            else
+            {
                 Console.WriteLine("No cumple las condiciones");
             }
 
-             if ((name == "Anartz" || name[0] == 'A' || name[0] == 'a' ) && (numberOne < numberTwo || numberTwo > numberThree)) {
+            if ((name == "Anartz" || name[0] == 'A' || name[0] == 'a') && (numberOne < numberTwo || numberTwo > numberThree))
+            {
                 Console.WriteLine("Cumple las condiciones númericas y del nombre (Parte 2)");
-            } else {
+            }
+            else
+            {
                 Console.WriteLine("No cumple las condiciones");
-            }*/
+            }
+
+
             // If ElseIf Else
             /*
            Sobresaliente (SB) = 9 o 10
@@ -56,7 +70,7 @@ namespace IfStructure
            Suficiente (SU) = 5
            Insuficiente (IN) = 1, 2, 3 o 4
            */
-            /*Console.WriteLine("Introduzca la nota entre 1 y 10 (incluidos):");
+            Console.WriteLine("Introduzca la nota entre 1 y 10 (incluidos):");
             int calification = Convert.ToInt32(Console.ReadLine());
             if (calification == 9 || calification == 10)
             {
@@ -77,7 +91,8 @@ namespace IfStructure
             else
             {
                 Console.WriteLine("Insuficiente (IN)");
-            }*/
+            }
+
             // If Anidado con If else if else
             Console.WriteLine("Introduzca la nota entre 1 y 10 (incluidos):");
             int calification = Convert.ToInt32(Console.ReadLine());
