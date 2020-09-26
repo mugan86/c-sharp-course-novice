d6043e23e24e6a3176a7c311d37f6b7691d7187b
Anartz Mugika Ledo <mugan86@gmail.com>
2020-09-26 22:47:52 +0200

    Añadidos nuevos ejercicios de Arrays

---
+++ Teoria/6-arrays/ArrayIndexOf/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrayIndexOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] sagas = {
+                "Tomb Raider", "Devil May Cry", "Resident Evil", "Resident Evil", "Mario",
+                "Final Fantasy", "Zelda", "Silent Hill", "Resident Evil", "Metal Gear", "Sonic",
+                "Tekken"
+            };
+
+            string searchString = "Resident Evil";
+            // Buscar Resident Evil en todo el array
+            int positionFind = Array.IndexOf(sagas, searchString); // 2
+
+            // Buscar Resident Evil a partir de la posición indice 3
+            positionFind = Array.IndexOf(sagas, searchString, 3); // 3
+
+            // Buscar Resident Evil a partir de la posición indice 4 durante 4 elementos
+            positionFind = Array.IndexOf(sagas, searchString, 4, 4); // -1
+            
+            // Buscar Resident Evil a partir de la posición indice 4 durante 4 elementos
+            positionFind = Array.IndexOf(sagas, searchString, 4, 6); // 8
+
+        }
+    }
+}
---
+++ Teoria/6-arrays/ArraySort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArraySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string videogamesCharacters = "Lara Croft,Nathan Drake,Mario,Ezio Auditore,Link,Jill Valentine";
+
+            string[] videogamesCharactersArray = videogamesCharacters.Split(',');
+
+            Array.Sort(videogamesCharactersArray);
+            // Ezio Auditore, Jill Valentine, Lara Croft, Link, Mario, Nathan Drake
+
+            // Modificar la posición index 1 y la 3 con nuevos valores
+            videogamesCharactersArray[1] = "Sackboy";
+            videogamesCharactersArray[3] = "Kirby";
+
+            // Ezio Auditore, Sackboy, Lara Croft, Kirby, Mario, Nathan Drake
+
+            // Ordenamos desde el index 1 durante 3 elementos (1, 2, 3 = indices)
+            Array.Sort(videogamesCharactersArray, 1, 3);
+
+            // Inicializar array de enteros
+            int [] numbersList = {100, 1, 2, 5, 750, 1000, 234};
+            // 1, 2, 5, 100, 234, 750, 1000 ==> .Sort(numbersList) ESTO NO LO APLICAMOS
+
+            Array.Sort(numbersList, 2, numbersList.Length - 2);
+
+            // 100, 1, 2, 5, 234, 750, 1000
+        }
+    }
+}
