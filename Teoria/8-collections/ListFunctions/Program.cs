1a2d32a31ee036c41e44072d8e8d2685494b7c26
Anartz Mugika Ledo <mugan86@gmail.com>
2020-10-04 07:40:35 +0200

    Aádidos más ejemplos prácticos

---
+++ Teoria/8-collections/ListFunctions/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.- Inicializar lista y añadir valores
+            List<int> numbers = new List<int>(){1, 2, 3}; // 1,2,3
+            numbers.Add(10); // 1,2,3,10
+            numbers.Add(700); // 1,2,3,10, 700
+
+            // 2.- Comprobar su longitud
+            int countElements = numbers.Count;
+
+            // 3.- Insertando información en posicion concreta (Insert)
+
+            numbers.Insert(1, 100); // 1, 100, 2,3,10, 700
+            numbers.Insert(3, 45); // 1, 100, 2,45, 3,10, 700
+
+            // 4.- Buscamos con "Contains" un elemento concreto
+            bool findOneValue = numbers.Contains(1);
+            bool findTwentyValue = numbers.Contains(20);
+            bool findElevenValue = numbers.Contains(11);
+            bool findTwentyOneValue = numbers.Contains(21);
+
+            // Estado actual de la lista 1, 100, 2,45, 3,10, 700
+            // 5.- Borramos elementos que existen y luego intentamos buscarlos
+            numbers.RemoveAt(0);
+             // Estado actual de la lista 100, 2,45, 3,10, 700
+            findOneValue = numbers.Contains(1);
+            numbers.RemoveAt(numbers.Count - 1);
+             // Estado actual de la lista 100, 2,45, 3,10
+            bool findValue = numbers.Contains(700);
+            numbers.Add(1000);
+            numbers.Add(1345);
+            numbers.Add(1000);
+            numbers.Add(-19);
+            // 100, 2,45, 3,10, 1000, 1345, 1000, -19
+            numbers.Remove(1000);
+            numbers.Remove(-1);
+            numbers.Remove(-50);
+            numbers.Remove(1345);
+            // 100, 2,45, 3,10, 1000, -19
+            numbers.Clear();
+            // VACIA
+            Console.WriteLine("Elementos en la lista {0}", numbers.Count);
+            numbers.Add(1345);
+            numbers.Add(1000);
+            // 1345, 1000
+            Console.WriteLine("Elementos en la lista {0}", numbers.Count); // 2
+        }
+    }
+}
---
+++ Teoria/8-collections/ListInitializeForEach/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListInitializeForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.- Creamos dos listas. La primera con números enteros y la segunda con strings
+            // Tiene que tener 3-4 elementos
+            List<int> numbers = new List<int>(){1, 23, -4, 56};
+            List<string> products = new List<string>(){"Patatas", "Manzana", "Refresco de Limón", "Macarrones"};
+            // 2.- Contar cuantos elementos en cada lista
+            Console.WriteLine("Elementos de la lista de números: {0}", numbers.Count);
+            Console.WriteLine("Elementos de la lista de productos: {0}", products.Count);
+            // 3.- Visualizar la información individualmente
+            
+            Console.WriteLine("Vamos a visualizar los elementos de la lista de enteros:");
+            foreach(int numberItem in numbers) {
+                Console.WriteLine(numberItem);
+            }
+
+            Console.WriteLine("Vamos a visualizar los elementos de la lista de productos:");
+            foreach(string productItem in products) {
+                Console.WriteLine(productItem);
+            }
+        }
+    }
+}
