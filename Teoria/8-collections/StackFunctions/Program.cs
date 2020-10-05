1cc00a1754c18056b9266c4025f57fce5d6aa3b4
Anartz Mugika Ledo <mugan86@gmail.com>
2020-10-05 20:37:18 +0200

    Pilas OK

---
+++ Teoria/8-collections/StackFunctions/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> pagesStack = new Stack<string>(new string[]{
+                "index", "blog", "article-1", "article-2"
+            });
+
+            // Añadir varios elementos más
+            pagesStack.Push("home");
+            pagesStack.Push("contact");
+            Console.WriteLine("Elementos en la pila: {0}", pagesStack.Count);
+            // Estado de la pila
+            // contact
+            // home
+            // article-2
+            // article-1
+            // blog
+            // index
+            // Usando "Contains"
+            Console.WriteLine("¿Tenemos el elemento \"contact\" en la pila ?: {0}", pagesStack.Contains("contact"));
+            Console.WriteLine("¿Tenemos el elemento \"homes\" en la pila ?: {0}", pagesStack.Contains("homes"));
+            Console.WriteLine("¿Tenemos el elemento \"blog\" en la pila ?: {0}", pagesStack.Contains("blog"));
+
+            // Eliminar 5 elementos
+            int i = 0;
+            while(i < 5) {
+                pagesStack.Pop();
+                Console.WriteLine(pagesStack.Peek());
+                i++;
+            }
+            // Estado de la pila
+            // index
+
+            // Vamos a añadir elementos para borrarlos con "Clear()"
+            pagesStack.Push("anartz");
+            pagesStack.Push("contact");
+            // Estado de la pila
+            // contact
+            // anartz
+            // index
+
+            pagesStack.Clear();
+
+            pagesStack.Push("anartz2");
+            pagesStack.Push("contact2");
+            // Estado de la pila
+            // contact2
+            // anartz2
+            pagesStack.Clear();
+
+            // COmprobamos de nuevo si existen
+            Console.WriteLine("¿Tenemos el elemento \"contact\" en la pila ?: {0}", pagesStack.Contains("contact"));
+            Console.WriteLine("¿Tenemos el elemento \"homes\" en la pila ?: {0}", pagesStack.Contains("homes"));
+            Console.WriteLine("¿Tenemos el elemento \"blog\" en la pila ?: {0}", pagesStack.Contains("blog"));
+        }
+    }
+}
---
+++ Teoria/8-collections/StackInitialize/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackInitialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inicializar las pilas
+
+            // Con array
+            Stack<int> numbersStack = new Stack<int>(new int[]{1, 2, 3, 4, 5});
+            // Como estaría la pila en este momento 
+            // 5
+            // 4
+            // 3
+            // 2
+            // 1
+
+            // Vacio
+            Stack<string> namesStack = new Stack<string>();
+            namesStack.Push("Anartz"); 
+            namesStack.Push("Jon");
+            namesStack.Push("Mikel");
+
+            // Mikel
+            // Jon
+            // Anartz
+            namesStack.Pop(); // Quitamos Mikel
+
+            // Jon
+            // Anartz
+
+            namesStack.Pop();
+
+            // Anartz
+
+            // Mostrar la información de los elementos de la pila
+            foreach(int elementNumber in numbersStack) {
+                Console.WriteLine(elementNumber);
+            }
+            // tiene que imprimir lo siguiente
+            // 5
+            // 4
+            // 3
+            // 2
+            // 1
+
+        }
+    }
+}
