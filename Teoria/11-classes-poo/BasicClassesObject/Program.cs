213192db84c6b43da607dc6d9076fca08bd062a6
Anartz Mugika Ledo <mugan86@gmail.com>
2020-11-07 20:38:52 +0100

    Añadido primer ejemplo de la sección Clases con creación de clases, objetos,...

---
+++ Teoria/11-classes-poo/BasicClassesObject/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BasicClassesObject
+{
+    class Program {
+        static void Main(string[] args)
+        {
+            Level theGreatWall = new Level();
+            theGreatWall.Game = "Tomb Raider 2";
+            theGreatWall.LocationName = "China";
+            theGreatWall.Name = "La Gran Muralla / The Great Wall";
+            theGreatWall.Order = 1;
+            theGreatWall.ShowDetails();
+            Level venecia = new Level();
+            venecia.Game = "Tomb Raider 2";
+            venecia.LocationName = "Venecia (Italia)";
+            venecia.Name = "Venecia";
+            venecia.Order = 2;
+            venecia.ShowDetails();
+            Level bartolisHideout = new Level();
+            bartolisHideout.Game = "Tomb Raider 2";
+            bartolisHideout.LocationName = "Venecia (Italia)";
+            bartolisHideout.Name = "El escondite de Bartoli";
+            bartolisHideout.Order = 3;
+            bartolisHideout.ShowDetails();
+            Level operaHouse = new Level();
+            operaHouse.Game = "Tomb Raider 2";
+            operaHouse.LocationName = "Venecia (Italia)";
+            operaHouse.Name = "La casa de la Opera";
+            operaHouse.Order = 4;
+            operaHouse.ShowDetails();
+        }
+    }
+    class Level
+    {
+        public string Name, Game, LocationName;
+        public int Order;
+
+        public void ShowDetails() {
+            Console.WriteLine("================== NIVELES======================");
+            Console.WriteLine("Nombre: {0}", Name);
+            Console.WriteLine("Juego: {0}", Game);
+            Console.WriteLine("Localización: {0}", LocationName);
+            Console.WriteLine("Número: {0}", Order);
+        }
+    }
+}
