67dab1ad2c87582716563117cf27a89d671bee1e
Anartz Mugika Ledo <mugan86@gmail.com>
2020-11-08 12:02:37 +0100

    Constructores empezando con los por defecto

--- Teoria/11-classes-poo/BasicClassesObject/Program.cs
+++ Teoria/11-classes-poo/BasicClassesObject/Program.cs
@@ -10,24 +10,28 @@ namespace BasicClassesObject
             theGreatWall.LocationName = "China";
             theGreatWall.Name = "La Gran Muralla / The Great Wall";
             theGreatWall.Order = 1;
+            theGreatWall.Secret = false;
             theGreatWall.ShowDetails();
             Level venecia = new Level();
             venecia.Game = "Tomb Raider 2";
             venecia.LocationName = "Venecia (Italia)";
             venecia.Name = "Venecia";
             venecia.Order = 2;
+            venecia.Secret = false;
             venecia.ShowDetails();
             Level bartolisHideout = new Level();
             bartolisHideout.Game = "Tomb Raider 2";
             bartolisHideout.LocationName = "Venecia (Italia)";
             bartolisHideout.Name = "El escondite de Bartoli";
             bartolisHideout.Order = 3;
+            bartolisHideout.Secret = true;
             bartolisHideout.ShowDetails();
             Level operaHouse = new Level();
             operaHouse.Game = "Tomb Raider 2";
             operaHouse.LocationName = "Venecia (Italia)";
             operaHouse.Name = "La casa de la Opera";
             operaHouse.Order = 4;
+            operaHouse.Secret = true;
             operaHouse.ShowDetails();
         }
     }
@@ -35,6 +39,7 @@ namespace BasicClassesObject
     {
         public string Name, Game, LocationName;
         public int Order;
+        public bool Secret;
 
         public void ShowDetails() {
             Console.WriteLine("================== NIVELES======================");
@@ -42,6 +47,7 @@ namespace BasicClassesObject
             Console.WriteLine("Juego: {0}", Game);
             Console.WriteLine("Localización: {0}", LocationName);
             Console.WriteLine("Número: {0}", Order);
+            Console.WriteLine("Secreto: {0}", Secret);
         }
     }
 }
---
+++ Teoria/11-classes-poo/Constructors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Person {
+        
+        public string Name;
+
+        public Person() {
+            Name = "Anartz Mugika";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pOne = new Person();
+            Console.WriteLine(pOne.Name);
+            Person pTwo = new Person();
+            pTwo.Name = "Mikel";
+            Console.WriteLine(pTwo.Name);
+        }
+    }
+}
