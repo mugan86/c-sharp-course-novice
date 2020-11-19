ea87b98db2c94e8f6c2048fea856a505934d906e
Anartz Mugika Ledo <mugan86@gmail.com>
2020-11-19 21:53:32 +0100

    Finish files

---
+++ Teoria/12-files/ExceptionsFiles/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionsFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fileElement;
+            try {
+                // Vamos a leer el fichero
+                // Carga fichero de ruta seleccionada
+                fileElement = File.OpenText("5-countries.txt");
+                string line = "";
+                do {
+                    line = fileElement.ReadLine();
+                    if (line != null) {
+                        Console.WriteLine(line);
+                    }
+                } while (line != null);
+
+            } catch (PathTooLongException) {
+                Console.WriteLine("El path o ruta de este fichero es demasiado largo. Procura añadir un path más corto");
+            } catch (FileNotFoundException) {
+                Console.WriteLine("El fichero seleccionado no es correcto por no encontrarse en la ruta especifada");
+            } catch (Exception exc) {
+                Console.WriteLine("Excepción: {0}", exc.Message);
+            }
+        }
+    }
+}
---
+++ Teoria/12-files/FileExists/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace FileExists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string curFile = @"../../../data/1-mario-saga-game.txt";
+            string curFile = @"/Users/anartz/Desktop/curso_c#/data/6-tomb-raider-saga-games.txt";
+            Console.WriteLine(File.Exists(curFile) ? "Fichero existe." : "Fichero no existe.");
+        }
+    }
+}
