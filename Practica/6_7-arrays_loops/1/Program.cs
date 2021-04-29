9b8ef8ce2501fb4d64347f6a46cf1be84ecf3e1f
Anartz Mugika Ledo <mugan86@gmail.com>
2021-04-29 16:23:32 +0200

    Ejercicios Arrays / Bucles en proceso

---
+++ Practica/6_7-arrays_loops/1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Crear array de 5 elementos para almacenar números
+            int[] numbersArray = new int[5];
+            int i;
+            Console.Write("\n\nAñadir y mostrar los elementos del array:\n");
+            Console.Write("-----------------------------------------\n");
+
+            Console.Write("Introduce 5 elementos en el array :\n");
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write("Elemento - {0} : ", i + 1);
+                // Convertimos lo introducido en un entero
+                numbersArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nLos elementos del array son: ");
+            for (i = 0; i < 5; i++)
+            {
+                if (i == 4)
+                {
+                    Console.Write("{0} ", numbersArray[i]);
+                }
+                else
+                {
+                    Console.Write("{0},  ", numbersArray[i]);
+                }
+            }
+            Console.Write("\n");
+        }
+    }
+}
---
+++ Practica/6_7-arrays_loops/2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Crear array de 5 elementos para almacenar números
+            int[] numbersArray = new int[5];
+            int i = 0;
+            Console.Write("\n\nAñadir y mostrar los elementos del array:\n");
+            Console.Write("-----------------------------------------\n");
+
+            Console.Write("Introduce 5 elementos en el array :\n");
+            while (i < 5)
+            {
+                Console.Write("Elemento - {0} : ", i + 1);
+                // Convertimos lo introducido en un entero
+                numbersArray[i] = Convert.ToInt32(Console.ReadLine());
+                i++;
+            }
+            // Reseteamos el contador
+            i = 0;
+            Console.Write("\nLos elementos del array son: ");
+            while (i < 5)
+            {
+                if (i == 4)
+                {
+                    Console.Write("{0} ", numbersArray[i]);
+                }
+                else
+                {
+                    Console.Write("{0},  ", numbersArray[i]);
+                }
+                i++;
+            }
+            Console.Write("\n");
+        }
+    }
+}
